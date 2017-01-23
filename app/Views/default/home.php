7b83f032eedbe1c013feb1f3c7879eb7acd35b0d
biscuitsaler <durandmallory@live.fr>
2017-01-23 16:10:59 +0100

    Enlevement placeholder W

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -47,14 +47,4 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
-
-	<h2>Let's code.</h2>
-	<p>Vous avez atteint la page d'accueil. Bravo.</p>
-	<p>Et maintenant, RTFM dans <strong><a href="../docs/tuto/" title="Documentation de W">docs/tuto</a></strong>.</p>
-    <ul>
-        <li><a href="<?= $this->url('user_loginForm') ?>">Ce connecter</a></li>
-        <li><a href="<?= $this->url('admin_users') ?>">Ce connecter</a></li>
-        <li></li>
-    </ul>
-
 <?php $this->stop('main_content') ?>
