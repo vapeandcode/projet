76125d5d8c4e5a94cdf0f419c683578f41451ece
biscuitsaler <durandmallory@live.fr>
2017-01-23 16:31:19 +0100

    ajout api

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -47,4 +47,6 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
+<iframe width="600" height="450" frameborder="0" style="border:0"
+src="https://www.google.com/maps/embed/v1/search?q=cigarette%20electronique%20france&key=AIzaSyAA0dqfzPEHNEyK3oWlhyJxBbhwSBMDJKA" allowfullscreen></iframe>
 <?php $this->stop('main_content') ?>
