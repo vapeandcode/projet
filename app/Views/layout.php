c242a3cd869d8027377c924d9cc720a12d70f144
tontonyolo <tontonyolo@gmail.com>
2017-01-23 17:00:09 +0100

    test 3 log

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -51,7 +51,7 @@
                  </div>
                  <ul class="nav navbar-nav navbar-right">
                     <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                    <li><a href="pages/ecig.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><a href="<?= $this->url("user_loginForm"); ?>"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                 </ul>
             </div>
         </nav>
--- app/Views/user/login.php
+++ app/Views/user/login.php
@@ -4,8 +4,8 @@
 <h2>Login</h2>
 <p>Login</p>
 <form class="addArticle" action="<?= $this->url('user_loginSubmit') ?>" method="post">
-    <input type="text" name="username" placeholder="Pseudo">
-    <input type="password" name="password" placeholder="Mot de passe">
+    <input type="text" name="username" placeholder="Pseudo"></br>
+    <input type="password" name="password" placeholder="Mot de passe"></br>
     <input type="submit" class="AddButton" name="subLogin" value="Connection">
 </form>
 <?php $this->stop('main_content') ?>
