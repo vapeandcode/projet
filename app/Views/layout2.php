be6c0e3412b9ea8ee7e465e699fd7f51ff19e7de
biscuitsaler <durandmallory@live.fr>
2017-01-24 16:54:18 +0100

    ajout css design

---
+++ app/Views/layout2.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Code N Vape</title>
+    <!-- CSS -->
+    <!-- google fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+    <!-- files -->
+    <link href="public/assets2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="public/assets2/css/magnific-popup.css" rel="stylesheet">
+    <link href="public/assets2/css/owl.carousel.css" rel="stylesheet">
+    <link href="public/assets2/css/owl.carousel.theme.min.css" rel="stylesheet">
+    <link href="public/assets2/css/ionicons.css" rel="stylesheet">
+    <link href="public/assets2/css/main.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+</head>
+
+<body>
+    <!-- Site Header -->
+    <div class="site-header-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <a href="index.html"><img src="assets/images/logo.png" alt="logo"></a>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!-- Header -->
+    <section id="header" class="main-header">
+        <div class="container">
+            <div class="row">
+                <nav class="navbar navbar-default">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#site-nav-bar" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                    </div>
+                    <div class="collapse navbar-collapse" id="site-nav-bar">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="index.html">Home</a></li>
+                            <li><a href="about.html">Choisir son matériel</a></li>
+                            <li><a href="liquides.html">Liquides</a></li>
+                            <li><a href="faq.html">FAQ</a></li>
+                            <li><a href="acheter.html">Acheter son matériel</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+                    <!-- /.navbar-collapse -->
+                </nav>
+            </div>
+            <div class="intro row">
+                <div class="overlay"></div>
+                <div class="col-sm-6 col-sm-offset-6">
+                    <h2 class="header-quote">Guide de la vape</h2>
+                    <p>
+                        pour tous les niveaux
+                    </p>
+                    <h1 class="header-title">Conseils<br><span class="thin">Guide</span></h1>
+                </div>
+            </div>
+            <!-- /.intro.row -->
+        </div>
+        <!-- /.container -->
+        <div class="nutral"></div>
+    </section>
+    <!-- /#header -->
+    <!-- Product -->
+    <section id="product" class="product">
+        <div class="container section-bg">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="title-box">
+                        <h2 class="title">Bienvenue sur <span>Code N Vape</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="porduct-box">
+                        <img class="img-responsive" src="assets/images/debutant.jpg" alt="product">
+                        <h3 class="product-title">Débutant</h3>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="porduct-box">
+                        <img class="img-responsive" src="assets/images/moyen.jpg" alt="product">
+                        <h3 class="product-title">Moyen</h3>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="porduct-box">
+                        <img class="img-responsive" src="assets/images/expert.jpg" alt="product">
+                        <h3 class="product-title">Expert</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="classic-title">
+                        <div class="stiker">
+                            <h3 class="inner-stiker">Réflexion</h3>
+                        </div>
+                        <h3 class="outer-stiker">sur la création du site</h3>
+                        <div class="incline-div"></div>
+                    </div>
+                </div>
+                <div class="boxed">
+                    <div class="col-sm-4">
+                        <p class="p-large">
+                            Sed lobortis volutpat imperdiet. faci.Fusce nec arcu ac neque tincidunt rutru tristique feugiat purus, id semper nisl tin
+                            vitae.Roin lobortis porta mattis. Mauris tincidunurus nec viverra mattis. Nunc convallis massa
+                            at eleifend blandit. Donec interdum.
+                        </p>
+                    </div>
+                    <div class="col-sm-4">
+                        <p>
+                            Ead lobortis volutpat imperdiet. Nulla faci.Fusce nec arcu ac neque tincidunt rutrum. Pro tristique feugiat purus, id semper
+                            nisl tincidunt vitae.Roin lobortis porta mattis. Mauris tincidunt purus nec viverra mattis. Nunc
+                            convallis massa at eleifend blandit. Donec interdum, sem lacinia dignissime varius, nulla eros
+                            consectetur mauris.
+                        </p>
+                    </div>
+                    <div class="col-sm-4">
+                        <p>
+                            Sed lobortis volutpat imperdiet. Nulla faci.Fusce nec arcu ac neque tincidunt rutrum. Pro tristique feugiat purus, id semper
+                            nisl tincidunt vitae.Roin lobortis porta mattis. Mauris tincidunt purus nec viverra mattis. Nunc
+                            convallis massa at eleifend blandit. Donec interdum, sem lacinia dignissime varius, nulla eros
+                            consectetur mauris.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- History -->
+    <section id="history" class="history">
+        <div class="container section-bg">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="title-box">
+                        <p>Trouver quelque chose</p>
+                        <h2 class="title mt0">D'intéressant</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="boxed">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetuer adipiscing elit sed diam nonummynibh euismod tincidunt ut laoree Dolore magna aliquam
+                            erat volutpat.Lorem ipsum dolor sit amet consectetuer adipiscing elit sed diam nonummynibh euismod
+                            tincidunt ut laoree Dolore magna aliquam erat .
+                        </p>
+                    </div>
+                    <div class="col-sm-12">
+                        <img class="img-responsive" src="assets/images/history.png" alt="history">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Partner -->
+    <section id="partner" class="partner">
+        <div class="container section-bg">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="title-box">
+                        <p>Réseaux sociaux</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="boxed">
+                    <div class="col-sm-12">
+                        <div id="partner-slider" class="owl-carousel">
+                            <div class="item">
+                                <img src="assets/images/6.png" alt="partner">
+                            </div>
+                            <div class="item">
+                                <img src="assets/images/7.png" alt="partner">
+                            </div>
+                            <div class="item">
+                                <img src="assets/images/8.png" alt="partner">
+                            </div>
+                            <div class="item">
+                                <img src="assets/images/9.png" alt="partner">
+                            </div>
+                            <div class="item">
+                                <img src="assets/images/6.png" alt="partner">
+                            </div>
+                            <div class="item">
+                                <img src="assets/images/7.png" alt="partner">
+                            </div>
+                            <div class="item">
+                                <img src="assets/images/8.png" alt="partner">
+                            </div>
+                            <div class="item">
+                                <img src="assets/images/9.png" alt="partner">
+                            </div>
+                            <div class="item">
+                                <img src="assets/images/6.png" alt="partner">
+                            </div>
+                            <div class="item">
+                                <img src="assets/images/7.png" alt="partner">
+                            </div>
+                            <div class="item">
+                                <img src="assets/images/8.png" alt="partner">
+                            </div>
+                            <div class="item">
+                                <img src="assets/images/9.png" alt="partner">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer -->
+    <section id="footer-widget" class="footer-widget">
+        <div class="container header-bg">
+            <h2>Nos réseaux sociaux</h2>
+            <a href="https://twitter.com/?lang=fr">
+                <img class="social" src="<?= $this->assetUrl('img/twitter.png') ?>" alt="Logo Twitter">
+            </a>
+            <a href="https://www.facebook.com/">
+                <img class="social" src="<?= $this->assetUrl('img/facebook.png') ?>" alt="Logo Facebook">
+            </a>
+            <br>
+            <!-- <h3>Suivez / Aimez</h3> -->
+            <a href="https://www.youtube.com/?gl=FR&hl=fr">
+                <img class="social" src="<?= $this->assetUrl('img/youtube.png') ?>" alt="Logo Youtube">
+                <a href="https://www.instagram.com/">
+                    <img class="social" src="<?= $this->assetUrl('img/instagram.png') ?>" alt="Logo Instagram">
+                </a>
+            </a>
+            <!-- <h3>Regardez / Admirez</h3> -->
+            <p>2016 - 2017 Tout droits réservés ©</p>
+        </div>
+    </section>
+    <footer class="footer text-center">
+    </footer>
+    <!-- Scripts -->
+    <script src="assets/js/jquery-1.12.3.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
