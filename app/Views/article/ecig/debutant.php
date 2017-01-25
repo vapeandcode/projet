720a1baf34f5535b4518c06056ffef60a7c847e3
tontonyolo <tontonyolo@gmail.com>
2017-01-25 17:21:18 +0100

    ecig pages

---
+++ app/Views/article/ecig/debutant.php
@@ -0,0 +1,19 @@
+<?php $this->layout('layout', ['title' => 'ecig debutant']);
+$this->start('main_content');
+?>
+    <h2>E-CIG DEBUTANTS</h2>
+    <p>
+        Des kits complets prêts a l'emploi,c'est très simple,on ajoute le liquide et on vape!!
+    </p>
+
+<?php for ($i=0; $i<count($listeArticle); $i++) { ?>
+    <div class="debutant">
+        <p><?php echo $listeArticle[$i]['title']?></p>
+        <p><?php echo $listeArticle[$i]['picture']?></p>
+        <p><?php echo $listeArticle[$i]['schema']?></p>
+        <p><?php echo $listeArticle[$i]['description']?></p>
+        <p><?php echo $listeArticle[$i]['link']?></p>
+    </div>
+<?php } ?>
+
+<?php $this->stop('main_content') ?>
