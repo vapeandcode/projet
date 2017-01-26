c4404baa521f9e1b4fa09c02416808f9e5dec297
biscuitsaler <durandmallory@live.fr>
2017-01-26 14:20:27 +0100

    légères modif

--- app/Views/article/ecig/expert.php
+++ app/Views/article/ecig/expert.php
@@ -3,7 +3,7 @@ $this->start('main_content');
 ?>
     <h2>E-CIG EXPERTS</h2>
     <p>
-        Cette catégorie est destinée aux connaisseurs du monde de la vape.Les drippers et les atomiseurs sont reconstructibles et offrent des tas de réglages avancés.
+        Cette catégorie est destinée aux connaisseurs du monde de la vape.Les drippers et les matériaux sont reconstructibles et offrent des tas de réglages avancés.
     </p>
 
 <?php for ($i=0; $i<count($listeArticle); $i++) { ?>
