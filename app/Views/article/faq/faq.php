b5e68a1a53fb75e16a4f6796a02a74ade70033c2
biscuitsaler <durandmallory@live.fr>
2017-01-27 17:47:01 +0100

    Ajout jquery ui effet dropdown faq

--- app/Views/article/faq/faq.php
+++ app/Views/article/faq/faq.php
@@ -1,39 +1,83 @@
 <?php $this->layout('layout', ['title' => 'Faq']) ?>
 
 <?php $this->start('main_content') ?>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+ <script>
+  $( function() {
+    var icons = {
+      header: "ui-icon-circle-arrow-e",
+      activeHeader: "ui-icon-circle-arrow-s"
+    };
+    $( "#accordion" ).accordion({
+      icons: icons
+    });
+    $( "#toggle" ).button().on( "click", function() {
+      if ( $( "#accordion" ).accordion( "option", "icons" ) ) {
+        $( "#accordion" ).accordion( "option", "icons", null );
+      } else {
+        $( "#accordion" ).accordion( "option", "icons", icons );
+      }
+    });
+  } );
+  </script>
 <h2>Foire aux questions</h2>
-<h3>La cigarette électronique c'est quoi ?<h3>
+<p> Cliquez sur la question pour voir la réponse apparaître</p>
+<div id="accordion">
+<h3>La cigarette électronique c'est quoi ?</h3>
+<div>
 <p>La cigarette électronique reproduit les sensations d'une cigarette classique. Elle est composée d'une batterie et d'un clearomiseur permettant d'accueillir le eliquide destiné à être vaporisé. Lors de l'aspiration, le eliquide, mélangé à l'air inspiré, est diffusé sous forme de vapeur. reproduisant la fumée d'une cigarette.</p>
 <p>Selon la réglementation de l’AFSSAPS, nous vous rappelons que la cigarette électronique n’est pas un médicament, qu’elle n’est pas vendue pour arrêter de fumer et est interdite aux moins de 18 ans.</p>
+</div>
 <h3>Quels sont les avantages de la cigarette éléctronique ?</h3>
+<div>
 <p>Pas de combustion, donc pas de goudron, uniquement de la vapeur aromatisée au goût et au dosage de nicotine de votre choix.</p>
 <p>Très économique, avec la cigarette electronique, votre budget est divisé au minimum par 5.</p>
 <p>Oubliez cendriers, briquets et odeurs de tabac.</p>
 <p>Vous gardez la même gestuelle , les mêmes sensations qu'avec une cigarette de tabac.</p>
+</div>
 <h3>Comment fonctionne une cigarette éléctronique ?</h3>
+<div>
 <p>La cigarette électronique est l'association d'une batterie et d'un clearomiseur, lui-même composé d'un réservoir et d'un système de vaporisation:la résistance.</p>
 <p>La batterie est le dispositif alimentant en énergie électrique la résistance de votre cigarette electronique. Deux points importants sont à prendre en compte dans le choix de votre kit de cigarette électronique: l'autonomie de la batterie et sa puissance, c'est à dire sa capacité à produire de la vapeur.</p>
 <p>Le clearomiseur est composé d'un réservoir, d'une résistance (composé d'un fil résistif et d'une mèche coton) et d'un embout buccal (drip tip). Les mèches de la résistance transportent par capillarité le eliquide contenu dans le réservoir jusqu'au fil résistif. La pression sur le bouton d'activation de la batterie provoque la chauffe du fil résistif et vaporise le eliquide en contact avec celui-ci. La vapeur produite remonte jusqu'à l'embout buccal (drip tip) et est inhalé par l'utilisateur. La taille du réservoir c'est à dire l'autonomie en eliquide est un critère à prendre en compte dans le choix de votre cigarette électronique.</p>
 <p>La résistance est un consommable qu'il faut changer régulièrement (en moyenne 1 résistance a une durée de vie de 15 jours).</p>
+</div>
 <h3>Pourquoi passer à la cigarette éléctronique ?</h3>
+<div>
 <p>Parce que vous êtes fumeur et que vous souhaitez vous libérer de l’addiction au tabac et à la cigarette. Si vous ne fumez pas, ne vapotez pas.
 Une étude récente réalisée par le cabinet d'audit financier Ernst & Young sur le marché de la cigarette électronique à mis en évidence que la première motivation des fumeurs est la protection de la santé. 51% des vapoteurs utilisent la cigarette électronique parce qu'elle est moins nocive que la cigarette classique. 49 % parce que la cigarette électronique les aide à réduire leur consommation de cigarette. Le prix est aussi une motivation en particulier dans les pays ou le tabac est fortement taxé.
 Source: vapingpost.com</p>
+</div>
 <h3>La cigarette éléctronique est-elle plus nocive que le tabac ?</h3>
+<div>
 <p>Contrairement à la cigarette, avec la cigarette électronique il n’y a pas de production de goudron ni de combustion. Or, c’est la combustion qui est responsable de la toxicité de la cigarette classique. Selon l'organisme Public Health England, la cigarette électronique est 95% moins nocive que la cigarette, contrairement à ce qu'affirmait l'OMS en 2014.</p>
+</div>
 <h3>On ne sait pas ce qu'il y a dans les cigarettes éléctronique ?</h3>
+<div>
 <p>Du propylene glycone , de la glycérine végetale , de la nicotine et de l'eau.</p>
 <p>On sait parfaitement de quoi est fait une cigarette électronique et un liquide pour cigarette électronique. La loi impose en France un étiquetage précis surtout pour les liquides. Les fabricants et vendeurs font un descriptif le plus complet possible pour chaque produit sur leur site. En boutique, il suffit de questionner le vendeur.</p>
+</div>
 <h3>Quels sont les avantages de la cigarette électronique sur ma santé ?</h3>
+<div>
 <p>Un vapoteur retrouve au bout de quelques jours son souffle, son appétit, le goût, son teint est moins gris. Les infections respiratoires chroniques diminuent. Cependant les liquides avec nicotine restent proscrits pour les femmes enceintes.</p>
+</div>
 <h3>La cigarette électronique est-elle nuisible à mon entourage ?</h3>
+<div>
 <p>De nombreuses études récentes montrent l’absence de nicotine dans la vapeur expirée par un vapoteur, contrairement à la fumée de cigarette.</p>
+</div>
 <h3>La cigarette électronique est-elle un moyen de sevrage à la cigarette classique ?</h3>
+<div>
 <p>De plus en plus d’études démontrent son efficacité pour la réduction des risques (cf. le rapport sur la Réduction des Risques et des Dommages d’avril 2016 publié par la Fédération Française d’Addictologie). Avec la cigarette électronique il n’y a pas de combustion de nicotine au d’autres produits. Les risques sont diminués de 95% selon certains experts. Enfin, de nombreux vapoteurs témoignent de leur diminution ou de leur arrêt complet de la cigarette après le passage à la cigarette électronique.</p>
+</div>
 <h3>Pourquoi y a t-il de "grosses" cigarettes électroniques ?</h3>
+<div>
 <p>Il s’agit des mods qui sont des modèles ayant une autonomie de batterie importante pour vapoter plus longtemps et une puissance élevée pour produire beaucoup de vapeur. Ce matériel n’est pas destiné à un public débutant car il demande de solides connaissances sur les résistances et l’utilisation des accus. Il existe les mods boxs et les mods tubulaires. Les mods boxs de taille mini sont plus adaptés au vapoteur débutant.</p>
+</div>
 <h3>Est-ce que je peux utiliser ma cigarette électronique n'importe où ?</h3>
+<div>
 <p>Il est interdit depuis janvier 2016 de vapoter sur les lieux de travail et certains endroits publics dont le règlement le stipule (transports en commun par exemple). La législation est encore assez vague à ce sujet.</p>
+</div>
 <?php $this->stop('main_content') ?>
 
 
