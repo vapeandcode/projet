26a0946164486d30e39fb0a0509b04bd565dd435
TristanParcelier <parceliert@gmail.com>
2017-01-30 23:47:00 +0100

    ajout home ecig

--- app/Views/article/ecig/home.php
+++ app/Views/article/ecig/home.php
@@ -7,31 +7,34 @@ $this->start('main_content');
     <p>Bien choisir son kit ecig est essentiel !!<p/>
     </div>
     <div class="grid">
-        <figure class="effect-honey">
-            <figcaption>
-                <h2>Débutants</h2>
-                <a href="<?= $this->url("ecig_debutant"); ?>"><img src="<?= $this->assetUrl('img\img_article\boxegoaio.jpg') ?>" alt="e-cigarette-debutant-box-ego-aio"/> </a>
-            </figcaption>
-        </figure>
-    </div>
+        <div class="row">
+            <div class="col-md-4">
+                <figure class="effect-honey">
+                    <figcaption>
+                        <h2>Débutants</h2>
+                        <a href="<?= $this->url("ecig_debutant"); ?>"><img src="<?= $this->assetUrl('img\img_article\boxegoaio.jpg') ?>" alt="e-cigarette-debutant-box-ego-aio"/> </a>
+                    </figcaption>
+                </figure>
+            </div>
 
-    <div class="grid">
-        <figure class="effect-honey">
-            <figcaption>
-            <h2>Connaisseurs</h2>
-                <a href="<?= $this->url("ecig_intermediaire"); ?>"><img src="<?= $this->assetUrl('img\img_article\istickpowernano.jpg') ?>" alt="e-cigarette-intermédiaire-i-stick-powernano"/> </a>
-            </figcaption>
-        </figure>
-    </div>
+            <div class="col-md-4">
+                <figure class="effect-honey">
+                    <figcaption>
+                        <h2>Connaisseurs</h2>
+                        <a href="<?= $this->url("ecig_intermediaire"); ?>"><img src="<?= $this->assetUrl('img\img_article\istickpowernano.jpg') ?>" alt="e-cigarette-intermédiaire-i-stick-powernano"/> </a>
+                    </figcaption>
+                </figure>
+            </div>
 
-    <div class="grid">
-        <figure class="effect-honey">
-            <figcaption>
-                <h2>Experts</h2>
-                <a href="<?= $this->url("ecig_expert"); ?>"><img src="<?= $this->assetUrl('img\img_article\kitboxalien.png') ?>" alt="e-cigarette-expert-kit-box-alien"/></a>
-            </figcaption>
-        </figure>
+            <div class="col-md-4">
+                <figure class="effect-honey">
+                    <figcaption>
+                        <h2>Experts</h2>
+                        <a href="<?= $this->url("ecig_expert"); ?>"><img src="<?= $this->assetUrl('img\img_article\kitboxalien.png') ?>" alt="e-cigarette-expert-kit-box-alien"/></a>
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
     </div>
-
 
 <?php $this->stop('main_content') ?>
