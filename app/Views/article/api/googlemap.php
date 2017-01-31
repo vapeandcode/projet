9ae5ccffdd150560aeaafe11790501638f9e6b6c
biscuitsaler <durandmallory@live.fr>
2017-01-31 14:57:21 +0100

    responsive google map

--- app/Views/article/api/googlemap.php
+++ app/Views/article/api/googlemap.php
@@ -2,7 +2,7 @@
 
 <?php $this->start('main_content') ?>
 <div class ="google">
-<iframe width="400" height="350" frameborder="0" style="border:0"
+<iframe width="100%" height="100%" frameborder="0" style="border:0"
 src="https://www.google.com/maps/embed/v1/search?q=cigarette%20electronique&key=AIzaSyAA0dqfzPEHNEyK3oWlhyJxBbhwSBMDJKA" allowfullscreen></iframe>
 </div>
 
